99226f2c2f1fcf9b87a969c2d421d85700c52c22
eugene-goldberg <eugene.goldberg117@gmail.com>
2016-04-22 14:59:50 -0500

    now bringing real data via

---
+++ public/js/controllers/scrubbedMeasureController-compiled.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var app = angular.module('inspinia');
+app.controller('scrubbedMeasureController', ['$scope', '$odataresource', 'toaster', function ($scope, $odataresource, toaster) {
+
+    $scope.ngData = [];
+
+    function getData() {
+        $odataresource("http://windows-10:8888/ChangeMeasure").odata().query(function (data) {
+            $scope.ngData = data;
+        }, function (err) {
+            console.log('There was an error: ', err);
+        });
+    }
+
+    getData();
+
+    //$scope.ngData = [
+    //    {Measure: "Changes Performed", Value: 50, ID: 1},
+    //    {Measure: "Changes Successful", Value: 43, ID: 1},
+    //    {Measure: "# Emergency Changes", Value: 27, ID: 1},
+    //    {Measure: "% Emergency Changes", Value: 29, ID: 1},
+    //    {Measure: "# of Process defect in failed changes", Value: 22, ID: 1},
+    //    {Measure: "# of urgent/expedited/aler changes", Value: 43, ID: 1},
+    //    {Measure: "% of urgent/expedited/aler changes", Value: 12, ID: 1},
+    //    {Measure: "# of unclosed changes", Value: 54, ID: 1},
+    //    {Measure: "% of unclosed changes", Value: 33, ID: 1},
+    //    {Measure: "% of failed changes", Value: 41, ID: 1},
+    //    {Measure: "# Changes lead to major incidents", Value: 48, ID: 1},
+    //    {Measure: "# Problems opened", Value: 19, ID: 1}
+    //];
+
+    $scope.selectedMeasure = "";
+    $scope.selectedValue = "";
+
+    $scope.mySelections = [];
+
+    $scope.showSelection = function () {
+        //console.log($scope.mySelections[0]);
+        $scope.selectedMeasure = $scope.mySelections[0].MeasureName;
+        $scope.selectedValue = $scope.mySelections[0].Value;
+    };
+
+    $scope.ngOptions = {
+        data: 'ngData',
+        selectedItems: $scope.mySelections,
+        multiSelect: false,
+        columnDefs: [{
+            field: 'MeasureName',
+            displayName: 'Measure'
+        }, {
+            field: 'Value',
+            displayName: 'Value'
+        }]
+    };
+
+    $scope.updateRecord = function () {
+        var index = $scope.ngData.findIndex(function (x) {
+            return x.MeasureName == $scope.selectedMeasure;
+        });
+        $scope.ngData[index].Value = $scope.selectedValue;
+    };
+}]);
+
+//# sourceMappingURL=scrubbedMeasureController-compiled.js.map
--- public/js/controllers/scrubbedMeasureController.js
+++ public/js/controllers/scrubbedMeasureController.js
@@ -2,24 +2,36 @@ var app = angular.module('inspinia');
 app.controller('scrubbedMeasureController',['$scope', '$odataresource','toaster',
     function($scope, $odataresource, toaster){
 
-        console.log('scrubbed measure controller');
+        $scope.ngData = [];
 
-        $scope.title = "NG Grid - Basic Example";
+        function getData(){
+            $odataresource("http://windows-10:8888/ChangeMeasure")
+                .odata()
+            .query(function(data){
+                    $scope.ngData = data;
+            },
+                 function(err) {
+                     console.log('There was an error: ', err);
+                 }
+            )
+        }
 
-        $scope.ngData = [
-            {Measure: "Moroni", Value: 50},
-            {Measure: "Teancum", Value: 43},
-            {Measure: "Jacob", Value: 27},
-            {Measure: "Nephi", Value: 29},
-            {Measure: "Joseph", Value: 22},
-            {Measure: "Monica", Value: 43},
-            {Measure: "Arnold", Value: 12},
-            {Measure: "Mark", Value: 54},
-            {Measure: "Amelia", Value: 33},
-            {Measure: "Jesica", Value: 41},
-            {Measure: "John", Value: 48},
-            {Measure: "Berg", Value: 19}
-        ];
+        getData();
+
+        //$scope.ngData = [
+        //    {Measure: "Changes Performed", Value: 50, ID: 1},
+        //    {Measure: "Changes Successful", Value: 43, ID: 1},
+        //    {Measure: "# Emergency Changes", Value: 27, ID: 1},
+        //    {Measure: "% Emergency Changes", Value: 29, ID: 1},
+        //    {Measure: "# of Process defect in failed changes", Value: 22, ID: 1},
+        //    {Measure: "# of urgent/expedited/aler changes", Value: 43, ID: 1},
+        //    {Measure: "% of urgent/expedited/aler changes", Value: 12, ID: 1},
+        //    {Measure: "# of unclosed changes", Value: 54, ID: 1},
+        //    {Measure: "% of unclosed changes", Value: 33, ID: 1},
+        //    {Measure: "% of failed changes", Value: 41, ID: 1},
+        //    {Measure: "# Changes lead to major incidents", Value: 48, ID: 1},
+        //    {Measure: "# Problems opened", Value: 19, ID: 1}
+        //];
 
         $scope.selectedMeasure = "";
         $scope.selectedValue = "";
@@ -28,18 +40,28 @@ app.controller('scrubbedMeasureController',['$scope', '$odataresource','toaster'
 
         $scope.showSelection = function(){
             //console.log($scope.mySelections[0]);
-            $scope.selectedMeasure = $scope.mySelections[0].Measure;
+            $scope.selectedMeasure = $scope.mySelections[0].MeasureName;
             $scope.selectedValue = $scope.mySelections[0].Value;
         };
 
         $scope.ngOptions = {
             data: 'ngData',
             selectedItems: $scope.mySelections,
-            multiSelect: false
+            multiSelect: false,
+            columnDefs: [
+                {
+                    field:'MeasureName',
+                    displayName:'Measure'
+                },
+                {
+                    field:'Value',
+                    displayName:'Value'
+                }
+            ]
         };
 
         $scope.updateRecord = function(){
-            var index = $scope.ngData.findIndex(x => x.Measure==$scope.selectedMeasure);
+            var index = $scope.ngData.findIndex(x => x.MeasureName==$scope.selectedMeasure);
             $scope.ngData[index].Value = $scope.selectedValue;
         };
 
